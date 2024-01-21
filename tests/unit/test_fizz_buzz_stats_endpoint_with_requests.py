9295166dbef4516d1e91edf972fcc6e4ad93e666
Khushal-Rathod-7035 <khushalrathod7035@gmail.com>
2024-01-21 12:22:10 +0530

    Updated documentation for fizz-buzz rest server.

--- tests/unit/test_fizz_buzz_stats_endpoint_with_requests.py
+++ tests/unit/test_fizz_buzz_stats_endpoint_with_requests.py
@@ -14,7 +14,7 @@ def test_fizz_buzz_stats_endpoint_with_requests():
 
     response = app.test_client().get('/rest/api/fizz-buzz-stats')
     data = json.loads(response.data.decode('utf-8'))
-    expected_result = {'id': data['id'], 'int1': 3, 'int2': 5, 'limit': 15,
-                       'str1': 'fizz', 'str2': 'buzz', 'hits': data['hits']}
+    expected_result = {'id': data['id'], 'int1': data['int1'], 'int2': data['int2'], 'limit': data['limit'],
+                       'str1': data['str1'], 'str2': data['str2'], 'hits': data['hits']}
     assert response.status_code == 200
     assert data == expected_result
